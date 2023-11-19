e33a4bba228088c29b2c768d572ce74fc5472abe
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-19 01:30:57 +0100

    up to @content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ function App() {
 	return (
 		<div className="App">
 		<h1>React Sass with Imports</h1>
-		<p>testing</p>
+		<p className="error-text spacing">This is an error.</p>
+		<p className="info-text spacing">This is an info.</p>
+		<p className="normal-text spacing">This is a normal text.</p>
+		<p className="faded-text spacing">This is a normal text.</p>
 		</div>
 	)
 }
