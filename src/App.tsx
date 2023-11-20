b535408af2426a21d47ecf953ba7daee733db047
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 03:01:16 +0100

    and finished

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ function App() {
 		<p className="info-text spacing">This is an info.</p>
 		<p className="normal-text spacing">This is a normal text.</p>
 		<p className="faded-text spacing">This is a normal text.</p>
+		<p className="ball-001 spacing"></p>
+		<p className="ball-002 shadow spacing"></p>
+		<p className="ball-003 spacing"></p>
+		<p className="ball-004 spacing"></p>
+		<p className="box-1 spacing"></p>
+		<p className="box-2 spacing"></p>
+		<p className="box-3 spacing"></p>
+		<p className="box-4 spacing"></p>
+		<p className="rectangle-red spacing"></p>
+		<p className="rectangle-blue spacing"></p>
+		<p className="rectangle-green spacing"></p>
+		<p className="rectangle-orange spacing"></p>
 		</div>
 	)
 }
